ab1eb110cf76f587541941ffc9ee5336b0ada41a
Timo Järve <timo.jarve.002@khk.ee>
2018-04-25 13:57:09 +0300

    Töötav skript

--- praks2/lisa_kasutaja.sh
+++ praks2/lisa_kasutaja.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
 #Lihtne kasutaja loomise skript
-#Loeb käsurealt soovitud kasutajanime
-read -p "Sisesta kasutajanimi: " nimi
-#Lisame kasutaja vastavalt sisendile
-useradd -m nimi -s /bin/bash
-echo "Kasutaja lisatud"
+
+
+if [ $(id -u) -eq 0 ]; then
+	#loeb kasutajanime
+	read -p "Sisesta kasutajanimi: " nimi
+	#kontrollib kas kasutaja on juba olemas
+	if [ $? -eq 0 ]; then
+		echo "$nimi kasutaja on juba olemas!"
+		exit 1 
+	#lisame kasutaja vastavalt sisendile
+	else
+		useradd -m $nimi -s /bin/bash
+		[ $? -eq 0 ] && echo "Kasutaja on lisatud!" || echo "Probleem kasutaja lisamisega"
+
+	fi
+	#veateade kui pole piisavalt õigusi
+else
+	echo "Kas oled root kasutaja?"
+	exit 1
+fi
