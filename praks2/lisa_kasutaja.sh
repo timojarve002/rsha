b5a4892042c2588a30846b8a834b380e0a09934d
Timo Järve <timo.jarve.002@khk.ee>
2018-04-25 13:12:08 +0300

    Skript poolik

--- praks2/lisa_kasutaja.sh
+++ praks2/lisa_kasutaja.sh
@@ -3,3 +3,6 @@
 #Lihtne kasutaja loomise skript
 #Loeb käsurealt soovitud kasutajanime
 read -p "Sisesta kasutajanimi: " nimi
+#Lisame kasutaja vastavalt sisendile
+useradd -m nimi -s /bin/bash
+echo "Kasutaja lisatud"
